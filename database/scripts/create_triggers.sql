b89b07be851af59af51a59786c04ffafc80e3ed9
Szlavikovics Máté <szlavimate@gmail.com>
2021-03-27 20:53:17 +0100

    Update Create triggers script

--- database/scripts/create_triggers.sql
+++ database/scripts/create_triggers.sql
@@ -1,7 +1,23 @@
-CREATE TRIGGER discount_valid_to
-AFTER INSERT ON discounts
+CREATE TRIGGER airlines_insert_into_countries
+AFTER INSERT ON airlines
 FOR EACH ROW
-WHEN (discounts.valid_to IS NULL)
 BEGIN
-    NEW.valid_to = (SELECT takeoff_date FROM flights INNER JOIN discounts ON flights.id = discounts.flight_id LIMIT 1);
-END
+    INSERT INTO countries (country_name) VALUES(:new.country);
+END;
+/
+
+CREATE TRIGGER airports_insert_into_countries
+AFTER INSERT ON airports
+FOR EACH ROW
+BEGIN
+    INSERT INTO countries (country_name) VALUES(:new.country);
+END;
+/
+
+CREATE TRIGGER users_insert_into_countries
+BEFORE INSERT ON users
+FOR EACH ROW
+BEGIN
+    INSERT INTO countries (country_name) VALUES(:new.country);
+END;
+/
