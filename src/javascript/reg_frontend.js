5270941efabfd799142be05b55608fa506a8c4b3
98bazsikaX <63373205+98bazsikaX@users.noreply.github.com>
2021-05-04 17:57:09 +0200

    reworked login.php

--- src/javascript/reg_frontend.js
+++ src/javascript/reg_frontend.js
@@ -1,39 +1,30 @@
-class reg_data{
-    /*
-    * Osztaly azert hogy konyebb legyen json objektumma alakitani a regisztracio adatait
-    * e :  {string} : email
-    * p :  {string} : password
-    * pa : {string} : password for checking
-    */
-    constructor(e,p,pa) {
-        this.email = e;
-        this.password = p;
-        this.passwordAgain = pa;
-    }
+/*
+* php-ban lesz megvalósítva a belépés és regisztráció session miatt!
+* countries api: https://restcountries.eu/rest/v2/all
+*
+* main: body onload függvénye
+* */
+function main() {
+    setCountries();
 }
 
-function register(){
-    /*Adatok felkeszitese*/
-    let email = document.getElementById("r_email").value;
-    let pwd = document.getElementById("r_password").value;
-    let passwordConf = document.getElementById("pwd_again").value;
-    let data = JSON.stringify(new reg_data(email,pwd,passwordConf));
 
-    /*API CALL*/
-    var request = new XMLHttpRequest();
-    request.open('POST','../php/API/login_API.php',false); //nem async mert nem akarok callbackel szenvedni
-    request.setRequestHeader("CONTENT-TYPE","application/json");
-    request.send(data);
+function setCountries(){
+    let request = new XMLHttpRequest();
+    request.open("GET","https://restcountries.eu/rest/v2/all");
 
-    if(request.status==200){
-        if(request.responseText){
-            console.log(request.responseText); //debug miatt
-            alert("Sikeres regisztráció!");
-        }else{
-            alert(request.responseText);
+    request.onreadystatechange = function () {
+        if(this.readyState === 4 && this.status===200){
+            let countries = JSON.parse(this.responseText);
+            let countryOption = document.getElementById("country");
+            countries.forEach(country =>{
+                let option = document.createElement("option");
+                option.text = country.name;
+                option.value = country.name;
+                countryOption.add(option);
+            });
         }
-    }else{
-       alert("Hiba tortent!");
-    }
+    };
+    request.send();
 
 }
--- src/php/login.php
+++ src/php/login.php
@@ -5,63 +5,65 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Belépés</title>
-    <link rel="stylesheet" href="../resources/stylesheets/global.css">
-    <script src="../javascript/reg_frontend.js"></script>
-    <style>
-        header{
-            width: 100%;
-            height: fit-content;
-        }
-        .common{
-            width: 80%;
-            border: 1px solid black;
-            overflow: hidden; /* will contain if #first is longer than #second */
-            display: inline-flex;
-        }
-        .reg{
-            width: 50%;
-            border: 1px solid red;
-        }
-        .login{
-            width: 50%;
-            border: 1px solid green;
-            
-        }
 
-    </style>
+<!--Stylesheets-->
+    <link rel="stylesheet" href="../resources/stylesheets/global.css">
+    <link rel="stylesheet" href="../resources/stylesheets/reg.css">
+
+<!--Scripts -->
+    <script src="../javascript/reg_frontend.js"></script>
+
 </head>
-<body>
+<body onload="main()">
     <header>Belépés</header>
-    <!--TODO: ha be van az illető jelentkezve akkor kijelentkezés only, vagy esetleg az adatainak szerkesztése,flex implementálása-->
+    <!--TODO: ha be van az illető jelentkezve akkor kijelentkezés only, vagy esetleg az adatainak szerkesztése,flex implementálása,aka session-->
     <div id="common">
         <div id="reg">
             <h2>Regisztráció</h2>
             <form>
                 <label for="r_email">Email:</label>
-                <input type="email" id="r_email" autocomplete="on">
+                <input type="email"  id="r_email" autocomplete="on" required>
                 <br>
                 <label for="r_password">Jelszó:</label>
-                <input type="password" id="r_password" autocomplete="on">
+                <input type="password" id="r_password" autocomplete="on" required>
                 <br>
                 <label for="pwd_again">Jelszó megerősítése:</label>
-                <input type="password" id="pwd_again" autocomplete="on">
+                <input type="password" id="pwd_again" autocomplete="on" required>
                 <br>
+                <label for="first_name">Keresztnév: </label>
+                <input type="text" id="first_name" name="first_name" required>
+                <br>
+                <label for="last_name">Vezetéknév: </label>
+                <input type="text" id="last_name" name="last_name" required>
+                <br>
+                <label for="phonenum">Telefonszám: </label>
+                <input type="tel" id="phonenum" name="phonenum" required>
+                <br>
+                <label for="bday">Születés dátuma: </label>
+                <input type="date" id="bday" name="bday" required>
+                <br>
+                <label for="country">Ország: </label>
+                <select id="country" name="country" required></select>
+                <br>
+                <label for="city">Város: </label>
+                <input type="text" name="city" id="city" required>
+                <br>
+                <label for="address">Utca/Házszám: </label>
+                <input type="text" id="address" name="address" required>
+                <input type="submit" value="Regisztráció">
             </form>
 
-            <button onclick="register()">Nyomj ram lol</button>
-                
         </div>
         <div id="login">
             <h2>Belépés</h2>
                 <form>
                     <label for="l_email">Email:</label>
-                    <input type="email" id="l_email" autocomplete="on">
+                    <input type="email" id="l_email" autocomplete="on" required>
                     <br>
                     <label for="l_password">Jelszó:</label>
-                    <input type="password" id="l_password" autocomplete="on">
+                    <input type="password" id="l_password" autocomplete="on" required><br>
+                    <input type="submit" value="Bejelentkezés">
                 </form>
-
-
         </div>    
     </div>
 </body>
