8ff02393f5551d8cb1f446535c8593316ea3ad7f
SarossyBence <79597030+SarossyBence@users.noreply.github.com>
2021-05-04 21:47:40 +0200

    rolunk és index upgrade
    lövik a pizsit holnap töri érettségi asszony mindjárt agyon ver XD

--- src/php/index.php
+++ src/php/index.php
@@ -6,17 +6,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SSS Tours</title>
     <link rel="stylesheet" href="../resources/stylesheets/global.css">
+    <link rel="stylesheet" href="../resources/stylesheets/index.css">
 
+    <script type="text/javascript">
+        function test_api(){
+
+    </script>
 </head>
 <body>
-    <header>
-        Üdvözöljük a Szlavikovics - Sárossy - Szombati utazási iroda weboldalán.
-    </header>
 
-        <?php
-        echo "<a href='login.php'>" . ((isset($_SESSION['user'])) ? "Kijelentkezés" : "Bejelentkezés") . "</a>" ;
-        ?>
+        <div class="topnav">
 
+            <a href="login.php">regisztráció/bejelentkezés</a>
+            <a href="admin.php">admin</a>
+            <a href="info.php">infók</a>
+            <a href="search.php">keresés</a>
+            <a href="buying.php">jegy vásárlás</a>
+            <a href="us.php">rólunk</a>
+
+        </div>
+
+    <br>
+    <br>
+    <br>
+    <h1> Üdvözöljük a Szlavikovics - Sárossy - Szombati utazási iroda weboldalán.</h1>
+  <!--   <button type="button" onclick="test_api()">TEST API</button> Valamiert nem megy a gomb wtf, test_api() funkcio meghivhato chrome consolebol -->
     <footer>
         All rights reserved
     </footer>
---
+++ src/php/us.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SSS Tours</title>
+    <link rel="stylesheet" href="../resources/stylesheets/global.css">
+    <link rel="stylesheet" href="../resources/stylesheets/us.css">
+
+
+</head>
+<body>
+<div class="Row">
+<div class="Column" >
+    <h1> Szlavikovics Máté</h1>
+    <img src="img/Máté.jpg" alt="Máté" width="350" height="400">
+
+
+</div>
+<div class="Column" >
+     <h1> Szombati Balázs</h1>
+    <img src="img/Balazs.jpg" alt="Balázs" width="300" height="400">
+
+</div>
+<div class="Column" >
+    <h1> Sárossy Bence</h1>
+    <img src="img/Bence.jpg" alt="Bence" width="300" height="400">
+
+</div>
+</div>
+</body>
+</html>
