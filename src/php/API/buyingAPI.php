2a04ca1c1a3f837a46dd125ecd82aa1c4898238c
SarossyBence <79597030+SarossyBence@users.noreply.github.com>
2021-05-08 12:32:40 +0200

    buying tákolási

--- src/php/API/buyingAPI.php
+++ src/php/API/buyingAPI.php
@@ -1,15 +1,20 @@
 <?php
-
+session_start();
 include_once '../dotenv/dotenv.php';
 include_once '../dotenv/dotenv.php';
 include '../php/functions/userdataForBuying.php';
+include '../php/functions/flightdataforbuying.php';
 load_env('../dotenv/.env');
 
 
 if(isset($_POST['buying'])){
-$email = $_POST['email'];
-$quantity = $_POST['quantity'];
+
 $state=1;
+    $email=$_SESSION['email'];
+    $first=$_SESSION['first'];
+    $last=$_SESSION['last'];
+    $birth=$_SESSION['birth'];
+
 $connection = oci_connect($_ENV['DATABASE_USERNAME'],$_ENV['DATABASE_PASSWORD'],$_ENV['DATABASE_LOCATION']);
 
 $query = "INSERT INTO PURCHASES (USER_EMAIL,FLIGHT_ID,QUANTITY,PURCHASE_STATE)VALUES(:r_email,:flight_id,:quantity,:state)";
