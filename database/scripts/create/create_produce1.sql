fa6a4f5c7140c7cbcc90507d744f26bb03cd0a03
SarossyBence <79597030+SarossyBence@users.noreply.github.com>
2021-05-09 19:50:42 +0200

    fix in procedures

--- database/scripts/create/create_produce1.sql
+++ database/scripts/create/create_produce1.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE PROCEDURE padd(mail IN users.email%TYPE, flight_id IN flights.id%TYPE, quantity IN purchases.quantity%TYPE) AS
+CREATE OR REPLACE PROCEDURE padd(mail users.email%TYPE, flight_id flights.id%TYPE, quantity  purchases.quantity%TYPE) AS
 
 BEGIN 
     INSERT INTO PURCHASES(USER_EMAIL,FLIGHT_ID,QUANTITY,PURCHASE_STATE) VALUES(mail,flight_id,quantity,'1');
